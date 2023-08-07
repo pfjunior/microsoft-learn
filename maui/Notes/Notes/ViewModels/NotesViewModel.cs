9e08948a61ccd780da0dadacfaedbbcd91521798
pfjunior <pf.junior89@hotmail.com>
2023-08-07 10:33:27 -0300

    tutorial mvvm - step 5

--- maui/Notes/Notes/ViewModels/NotesViewModel.cs
+++ maui/Notes/Notes/ViewModels/NotesViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Notes.ViewModels
+﻿namespace Notes.ViewModels
 {
 	internal class NotesViewModel
 	{
