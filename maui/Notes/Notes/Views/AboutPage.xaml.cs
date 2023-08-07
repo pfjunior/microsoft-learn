64d65e7c58911bd1a809185c72854d74c69a83ad
pfjunior <pf.junior89@hotmail.com>
2023-08-07 10:05:18 -0300

    tutorial mvvm - step 4

--- maui/Notes/Notes/Views/AboutPage.xaml.cs
+++ maui/Notes/Notes/Views/AboutPage.xaml.cs
@@ -1,5 +1,3 @@
-using Notes.Models;
-
 namespace Notes.Views;
 
 public partial class AboutPage : ContentPage
@@ -7,11 +5,5 @@ public partial class AboutPage : ContentPage
 	public AboutPage()
 	{
 		InitializeComponent();
-	}
-
-	private async void LearnMore_Clicked(object sender, EventArgs e)
-	{
-		if(BindingContext is About about)
-			await Launcher.Default.OpenAsync(about.MoreInfoUrl);
 	}
 }
