8bb782aba25add3520495dc203d8ff4adb212307
pjunior <pf.junior89@hotmail.com>
2024-01-03 14:56:55 -0300

    iniciando projeto

---
+++ FruitAPI/Data/FruitDb.cs
@@ -0,0 +1,23 @@
+using FruitAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FruitAPI.Data;
+
+class FruitDb : DbContext
+{
+    public FruitDb(DbContextOptions<FruitDb> options)
+        : base(options) { }
+
+    public DbSet<Fruit> Fruits => Set<Fruit>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Fruit>()
+            .HasData(
+            new Fruit { Id = 1, Name = "Apple", Instock = true },
+            new Fruit { Id = 2, Name = "Banana", Instock = false },
+            new Fruit { Id = 3, Name = "Orange", Instock = true }
+            );
+    }
+}
---
+++ FruitAPI/Models/Fruit.cs
@@ -0,0 +1,9 @@
+namespace FruitAPI.Models;
+
+public class Fruit
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool Instock { get; set; }
+
+}
