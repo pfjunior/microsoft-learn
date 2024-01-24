e34aafa9d1fca555e134102ff5b6cbd8937d354b
pjunior <pf.junior89@hotmail.com>
2024-01-24 18:21:40 -0300

    crack the code and reveal a secret with python and vs code - aula 04

--- python/python-basics/sleuth/decrypt.py
+++ python/python-basics/sleuth/decrypt.py
@@ -1,16 +1,24 @@
-print("Hello, Contosoville!")
+# print("Hello, Contosoville!")
 # This is a comment that won't be interpreted as a command.
 
 # Associate variable town with the value "Contosoville"
-town = "Contosoville"
+# town = "Contosoville"
 
 # Print a message about the secret location
-print( "The town I am looking for is " + town )
+# print( "The town I am looking for is " + town )
 
 # Define a power (function) to chant a phrase
-def chant( phrase ):
+# def chant( phrase ):
     # Glue three copies together and print it as a message
-    print( phrase + phrase + phrase )
+  #  print( phrase + phrase + phrase )
 
 # Invoke the power to chant on the phrase "Contosoville!"
-chant( "Contosoville!" )
+# chant( "Contosoville!" )
+
+def lasso_letter(letter, shift_amount):
+    letter_code = ord(letter.lower())
+    decoded_letter_code = letter_code + shift_amount
+    decoded_letter = chr(decoded_letter_code)
+    return decoded_letter
+
+print(lasso_letter("N", 13))
