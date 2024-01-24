aedca21af10532eee15e86de668556a739f1c0c3
pjunior <pf.junior89@hotmail.com>
2024-01-24 18:34:38 -0300

    crack the code and reveal a secret with python and vs code - aula 06

--- python/python-basics/sleuth/decrypt.py
+++ python/python-basics/sleuth/decrypt.py
@@ -1,10 +1,10 @@
-# Define a function to find the truth by shifting the letter by the specified amount
+# Define a function to find the truth by shifting the letter by a specified amount
 def lasso_letter( letter, shift_amount ):
     # Invoke the ord function to translate the letter to its ASCII code 
-    # Save the code to the letter_code variable
+    # Save the code value to the variable called letter_code
     letter_code = ord(letter.lower())
     
-    # The ASCII number representation of lowercase letter 'a'
+    # The ASCII number representation of lowercase letter a
     a_ascii = ord('a')
 
     # The number of letters in the alphabet
@@ -20,4 +20,24 @@ def lasso_letter( letter, shift_amount ):
     # Send the decoded letter back
     return decoded_letter
 
-print(lasso_letter("a", 2))
+# Define a function to find the truth in a secret message
+# Shift the letters in a word by a specified amount to discover the hidden word
+def lasso_word( word, shift_amount ):
+
+    # This variable is updated each time another letter is decoded
+    decoded_word = ""
+
+    # This for loop iterates through each letter in the word parameter
+    for letter in word:
+        # The lasso_letter() function is invoked with each letter and the shift amount
+        # The result (the decoded letter) is stored in a variable called decoded_letter
+        decoded_letter = lasso_letter(letter, shift_amount)
+
+        # The decoded_letter value is added to the end of the decoded_word value
+        decoded_word = decoded_word + decoded_letter
+
+    # The decoded_word is sent back to the line of code that invoked this function
+    return decoded_word
+
+# Try to decode the word "terra"
+print( "Shifting terra by 13 gives: \n" + lasso_word( "terra", 13 ) )
