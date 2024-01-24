46e205af8e2be9b6fc417692b127fd52dcf0ae0e
pjunior <pf.junior89@hotmail.com>
2024-01-24 19:19:55 -0300

    what is your super sleuth personality - aula 06

--- python/python-basics/sleuth/quiz.py
+++ python/python-basics/sleuth/quiz.py
@@ -1,12 +1,52 @@
 # ask the candidate a question
 activity = input( "How would you like to spend your evening?\n(A) Reading a book\n(B) Attending a party\n" )
-
-# print out which activity they chose
-print( f"You chose {activity}.")
-
 if activity == "A":
-    print( "Nice choice!" )
+    print( "Reading a book, nice choice!" )
 elif activity == "B":
-    print( "Sounds fun!" )
+    print( "Attending a party? Sounds fun!" )
 else:
     print("You must type A or B, let's just say you like to read.")
+    activity = "A"
+
+# ask the candidate a second question
+job = input( "What's your dream job?\n(A) Curator at the Smithsonian\n(B) Running a business\n" )
+if job == "A":
+    print( "Curator, nice choice!" )
+elif job =="B":
+    print( "Running a business? Sounds fun!" )
+else:
+    print("You must type A or B, let's just say you want to be a curator at the Smithsonian")
+    job = "A"
+
+# ask the candidate a third question
+value = input( "What's more important?\n(A) Money\n(B) Love\n" )
+if value == "A":
+    print( "Money, nice choice!" )
+elif value =="B":
+    print( "Love? Sounds fun!" )
+else:
+    print("You must type A or B, let's just say money is more important to you.")
+    value = "A"
+
+# ask the candidate a fourth question
+decade = input( "What's your favorite decade?\n(A) 1910s\n(B) 2010s\n" )
+if decade == "A":
+    print( "1910s, nice choice!" )
+elif decade =="B":
+    print( "2010s? Sounds fun!" )
+else:
+    print("You must type A or B, let's just say the 1910s is your favorite decade.")
+    decade = "A"
+
+# ask the candidate a fifth question
+travel = input( "What's your favorite way to travel?\n(A) Driving\n(B) Flying\n" )
+if travel == "A":
+    print( "Driving, nice choice!" )
+elif travel =="B":
+    print( "Flying? Sounds fun!" )
+else:
+    print("You must type A or B, let's just say your favorite way to travel is by driving")
+    travel = "A"
+
+# print out their choices
+print( f"You chose {activity}, then {job}, then {value}, then {decade}, then {travel}.")
