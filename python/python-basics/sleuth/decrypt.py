cdca444104a4e7c4eaa461738a01c5bfd8ac74ef
pjunior <pf.junior89@hotmail.com>
2024-01-24 18:36:06 -0300

    crack the code and reveal a secret with python and vs code - aula 07

--- python/python-basics/sleuth/decrypt.py
+++ python/python-basics/sleuth/decrypt.py
@@ -40,4 +40,10 @@ def lasso_word( word, shift_amount ):
     return decoded_word
 
 # Try to decode the word "terra"
-print( "Shifting terra by 13 gives: \n" + lasso_word( "terra", 13 ) )
+# print( "Shifting terra by 13 gives: \n" + lasso_word( "terra", 13 ) )
+
+# Try to decode the message
+print( "Shifting Ncevy by 13 gives: \n" + lasso_word( "Ncevy", 13 ) )
+print( "Shifting gpvsui by 25 gives: \n" + lasso_word( "gpvsui", 25 ) )
+print( "Shifting ugflgkg by -18 gives: \n" + lasso_word( "ugflgkg", -18 ) )
+print( "Shifting wjmmf by -1 gives: \n" + lasso_word( "wjmmf", -1 ) )
