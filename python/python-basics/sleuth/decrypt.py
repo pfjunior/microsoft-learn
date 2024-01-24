a9a7f5da643fb0521d10f2af1a421a6f43b32386
pjunior <pf.junior89@hotmail.com>
2024-01-24 18:28:43 -0300

    crack the code and reveal a secret with python and vs code - aula 05

--- python/python-basics/sleuth/decrypt.py
+++ python/python-basics/sleuth/decrypt.py
@@ -1,24 +1,23 @@
-# print("Hello, Contosoville!")
-# This is a comment that won't be interpreted as a command.
-
-# Associate variable town with the value "Contosoville"
-# town = "Contosoville"
-
-# Print a message about the secret location
-# print( "The town I am looking for is " + town )
-
-# Define a power (function) to chant a phrase
-# def chant( phrase ):
-    # Glue three copies together and print it as a message
-  #  print( phrase + phrase + phrase )
-
-# Invoke the power to chant on the phrase "Contosoville!"
-# chant( "Contosoville!" )
-
-def lasso_letter(letter, shift_amount):
+# Define a function to find the truth by shifting the letter by the specified amount
+def lasso_letter( letter, shift_amount ):
+    # Invoke the ord function to translate the letter to its ASCII code 
+    # Save the code to the letter_code variable
     letter_code = ord(letter.lower())
-    decoded_letter_code = letter_code + shift_amount
-    decoded_letter = chr(decoded_letter_code)
+    
+    # The ASCII number representation of lowercase letter 'a'
+    a_ascii = ord('a')
+
+    # The number of letters in the alphabet
+    alphabet_size = 26
+
+    # The formula to calculate the ASCII number for the decoded letter
+    # Take into account looping around the alphabet
+    true_letter_code = a_ascii + (((letter_code - a_ascii) + shift_amount) % alphabet_size)
+
+    # Convert the ASCII number to the character or letter
+    decoded_letter = chr(true_letter_code)
+
+    # Send the decoded letter back
     return decoded_letter
 
-print(lasso_letter("N", 13))
+print(lasso_letter("a", 2))
