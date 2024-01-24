fbb9fcc637241875f8777d95d4fb4beaa52b3f29
pjunior <pf.junior89@hotmail.com>
2024-01-24 19:13:37 -0300

    what is your super sleuth personality - aula 04

---
+++ python/python-basics/sleuth/quiz.py
@@ -0,0 +1,9 @@
+# ask the candidate a question
+activity = input( "How would you like to spend your evening?\n(A) Reading a book\n(B) Attending a party\n" )
+
+# print out which activity they chose
+print( f"You chose {activity}.")
+
+# if they chose reading a book
+if activity == "A":
+    print( "Nice choice!" )
