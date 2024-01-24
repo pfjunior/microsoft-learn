7fe7d8c7c13ffb6c7437916a7458ac5d87531b45
pjunior <pf.junior89@hotmail.com>
2024-01-24 19:21:19 -0300

    what is your super sleuth personality - aula 06

--- python/python-basics/sleuth/quiz.py
+++ python/python-basics/sleuth/quiz.py
@@ -50,3 +50,65 @@ else:
 
 # print out their choices
 print( f"You chose {activity}, then {job}, then {value}, then {decade}, then {travel}.")
+
+# create some variables for scoring
+sam_like = 0
+cam_like = 0
+kai_like = 0
+indy_like = 0
+
+# update scoring variables based on the activity choice
+if activity == "A":
+    sam_like = sam_like + 2
+    indy_like = indy_like + 2
+    kai_like = kai_like + 2
+else:
+    cam_like = cam_like + 1
+    indy_like = indy_like + 1
+
+# update scoring variables based on the job choice
+if job == "A":
+    sam_like = sam_like + 2
+    indy_like = indy_like + 2
+    cam_like = cam_like - 1
+else:
+    sam_like = sam_like - 1
+    kai_like = kai_like + 2
+    indy_like = indy_like + 1
+
+# update scoring variables based on the value choice
+if value == "A":
+    sam_like = sam_like - 1
+    kai_like = kai_like + 1
+else:
+    sam_like = sam_like + 2
+    cam_like = cam_like + 2
+    indy_like = indy_like + 1
+
+# update scoring variables based on the decade choice
+if decade == "A":
+    cam_like = cam_like + 2
+    sam_like = sam_like + 2
+else:
+    kai_like = kai_like + 1
+    indy_like = indy_like + 2
+
+# update scoring variables based on the travel choice
+if travel == "A":
+    sam_like = sam_like - 2
+    kai_like = kai_like + 1
+    indy_like = indy_like - 1
+else:
+    sam_like = sam_like + 1
+    cam_like = cam_like + 1
+    kai_like = kai_like - 1
+
+# print the results depending on the score
+if sam_like >= 3:
+    print( "You're most like Sharp-Eyed Sam!" )
+elif cam_like >= 3:
+    print( "You're most like Curious Cam!" )
+elif kai_like >= 3:
+    print( "You're most like Keen Kai!" )
+else:
+    print( "You're most like Inquisitive Indy!" )
