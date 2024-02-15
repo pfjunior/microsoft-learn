f89c347db15518da519def7fa44526c7b83c759a
pjunior <pf.junior89@hotmail.com>
2024-02-15 14:36:51 -0300

    python for beginners - use boolean logic in python

---
+++ python/python-beginners/boolean-expressions.py
@@ -0,0 +1,17 @@
+# Example using if, elif and else
+object_size = 10
+
+if object_size > 5:
+    print("We need to keep an eye on this object")
+else:
+    print("Object poses no threat")
+
+
+# Example using "and" and "or" operators
+object_size = 10
+proximity = 9000
+
+if object_size > 5 and proximity < 10000:
+    print("Evasive maneuvers required")
+else:
+    print("Object poses no threat")
