6dcb6eeab41ee034fba2110af6b0b825cdc33a83
pjunior <pf.junior89@hotmail.com>
2024-02-16 11:26:40 -0300

    python for beginners - use strings in python

---
+++ python/python-beginners/strings.py
@@ -0,0 +1,20 @@
+text = """Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth. 
+On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
+
+setences = text.split(". ")
+# print(setences)
+
+# for sentence in setences:
+    # if "temperature" in sentence:
+        # print(sentence)
+
+name = "Ganymede"
+planet = "Mars"
+gravity = "1.43"
+
+template = """Gravity Facts about {name}
+--------------------------
+Planet Name: {planet}
+Gravity on {name}: m/s2"""
+
+print(template.format(name=name, planet=planet, gravity=gravity))
